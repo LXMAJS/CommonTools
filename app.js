8ba7d55783a59df94c0045461adb9ef7eb6da2b3
黎进 <lijin@lijindeMacBook-Pro.local>
2018-03-14 22:30:58 +0800

    my first nodejs demo

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res){
+    res.send('Ritsu Yan');
+});
+
+app.get('/about', function (req, res) {
+    res.send('the node course');
+});
+
+app.get('/user', function (req, res) {
+    res.send({
+        'id':'1',
+        'name': 'lijin'
+    });
+});
+
+app.use(function (req, res, next) {
+    res.status(404);
+    res.send('404 -- not found');
 });
 
 var server = app.listen(3000, function() {
