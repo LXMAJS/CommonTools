e67db495832709c9ccc92094ef512924d6b88b48
lijin <lijin>
2018-03-28 21:05:45 +0800

    cpmmit db

--- models/book.js
+++ models/book.js
@@ -1,35 +1,35 @@
-// /**
-//  * @author : lxmajs
-//  */
+/**
+ * @author : lxmajs
+ */
 
-// 'use strict'
+'use strict'
 
-// const Sequelize = require('sequelize');
-// var sequelize = require('../db/sequelize');
+const Sequelize = require('sequelize');
+var sequelize = require('../db/sequelize');
 
-// var Book = sequelize.define('Book', {
-//     id: {
-//         type: Sequelize.BIGINT,
-//         primaryKey: true
-//     },
-//     title: Sequelize.STRING(255),
-//     coverimg_url: Sequelize.STRING(255),
-//     create_time: Sequelize.DATETIME
-// }, {
-//         timestamps: false
-//     });
+var Book = sequelize.define('Book', {
+    id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true
+    },
+    title: Sequelize.STRING(255),
+    coverimg_url: Sequelize.STRING(255),
+    create_time: Sequelize.DATETIME
+}, {
+        timestamps: false
+    });
 
-// function addBook(book) {
-//     var now = Date.now();
-//     (async () => {
-//         var newBook = await Book.create({
-//             id: 'd-' + now,
-//             title: book.title,
-//             coverimg_url: book.coverimg_url,
-//             create_time: now
-//         });
-//         console.log('created: ' + JSON.stringify(newBook));
-//     })();
-// }
+function addBook(book) {
+    var now = Date.now();
+    (async () => {
+        var newBook = await Book.create({
+            id: 'd-' + now,
+            title: book.title,
+            coverimg_url: book.coverimg_url,
+            create_time: now
+        });
+        console.log('created: ' + JSON.stringify(newBook));
+    })();
+}
 
-// module.exports = Book;
+module.exports = Book;
